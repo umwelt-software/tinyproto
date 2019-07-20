8b7303dfdce64e1fe12b05f8dd6cde47e36fbda1
Nico Maas <mail@nico-maas.de>
2019-07-20 16:26:08 +0200

    New Build Files and Arduino Lib Files
    Added new gen_arduino_release files for win/lin and library files based on the work of santicastro

--- gen_arduino_release.bat
+++ gen_arduino_release.bat
@@ -1,15 +1,23 @@
 @echo off
 
-rmdir /S /Q releases\arduino\TinyProto\src
-rmdir /S /Q releases\arduino\TinyProto\examples
-rmdir /S /Q releases\arduino\TinyProto\tools
-mkdir releases\arduino\TinyProto\src\proto
-xcopy /Q /Y /E src\arduino\* releases\arduino\TinyProto\
-del releases\arduino\TinyProto\library.properties.in
-xcopy /Q /Y /E src\lib\* releases\arduino\TinyProto\src\proto\
-xcopy /Q /Y /E tools releases\arduino\TinyProto\tools\
-copy inc\*.h releases\arduino\TinyProto\src\proto\
-copy inc\serial_api.h releases\arduino\TinyProto\src\proto\serial\
-copy inc\os\arduino\*.h releases\arduino\TinyProto\src\proto\
+rmdir /S /Q releases
+mkdir releases\arduino\TinyProto\src
+
+xcopy /Q /Y /E examples releases\arduino\TinyProto\examples\
+
+xcopy /Q /Y /E src\*.cpp releases\arduino\TinyProto\src\
+xcopy /Q /Y /E src\*.h releases\arduino\TinyProto\src\
+xcopy /Q /Y /E src\proto releases\arduino\TinyProto\src\proto\
+
+rmdir /S /Q releases\arduino\TinyProto\src\proto\hal\linux
+rmdir /S /Q releases\arduino\TinyProto\src\proto\hal\mingw32
+del releases\arduino\TinyProto\src\proto\hal\tiny_defines.h
+move releases\arduino\TinyProto\src\proto\hal\arduino\* releases\arduino\TinyProto\src\proto\hal\ 
+rmdir /S /Q releases\arduino\TinyProto\src\proto\hal\arduino
+
+copy library.json releases\arduino\TinyProto\
+copy library.properties releases\arduino\TinyProto\
+copy LICENSE releases\arduino\TinyProto\
+copy keywords.txt releases\arduino\TinyProto\
 
 echo "arduino package build ... [DONE]"
---
+++ gen_arduino_release.sh
@@ -0,0 +1,28 @@
+#/bin/sh
+OUTPUTPATH=releases/arduino/TinyProto
+OUTPUTZIP=releases/arduino/TinyProto.zip
+
+rm -rf $OUTPUTPATH
+rm -rf $OUTPUTZIP
+
+mkdir -p $OUTPUTPATH/src
+cp src/*.cpp $OUTPUTPATH/src
+##cp src/*.c $OUTPUTPATH/src
+##cp src/*.hpp $OUTPUTPATH/src
+cp src/*.h $OUTPUTPATH/src
+cp -R examples $OUTPUTPATH
+cp -R src/proto $OUTPUTPATH/src
+rm -rf $OUTPUTPATH/src/proto/hal/linux
+rm -rf $OUTPUTPATH/src/proto/hal/mingw32
+rm -rf $OUTPUTPATH/src/proto/hal/tiny_defines.h
+mv $OUTPUTPATH/src/proto/hal/arduino/* $OUTPUTPATH/src/proto/hal/
+rm -rf $OUTPUTPATH/src/proto/hal/arduino
+cp library.json $OUTPUTPATH
+cp library.properties $OUTPUTPATH
+cp LICENSE $OUTPUTPATH
+cp keywords.txt $OUTPUTPATH
+
+
+(cd releases/arduino/; zip -r TinyProto.zip TinyProto)
+
+echo "arduino package build ... [DONE]"
