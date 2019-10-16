bb28a1ba82c36ef99b43e833ea8f6178d8fde97f
Aleksei Dynda <alexey.dynda@gmail.com>
2019-10-16 12:53:04 +1000

    Generating documentation

--- docs/search/typedefs_4.js
+++ docs/search/typedefs_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['tiny_5ffd_5fhandle_5ft',['tiny_fd_handle_t',['../tiny__fd_8h.html#a91e6b79431fe38570fb102701ef0b7e8',1,'tiny_fd.h']]]
+  ['tiny_5ffd_5fhandle_5ft',['tiny_fd_handle_t',['../group__FULL__DUPLEX__API.html#ga91e6b79431fe38570fb102701ef0b7e8',1,'tiny_fd.h']]]
 ];
