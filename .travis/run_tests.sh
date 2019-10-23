d12343275801821be848e04044387b57f255ef07
Aleksei Dynda <alexey.dynda@gmail.com>
2019-10-23 14:22:50 +1000

    Changes to communication tests

--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -17,10 +17,11 @@ create_terminals
 
 # Full duplex tests
 # Run sideB in listen/loopback mode
-./bld/tiny_loopback -p /tmp/sideB -t fd -r &
+./bld/tiny_loopback -p /tmp/sideB -t fd -r -w 7 &
 sleep 0.5
 # Run tests on sideA for 15 seconds
-./bld/tiny_loopback -p /tmp/sideA -t fd -g -r
+# Use window 4 for now, need to solve overload connection issues
+./bld/tiny_loopback -p /tmp/sideA -t fd -g -r -w 4
 if [ $? -ne 0 ]; then
     close_terminals_and_exit 1
 fi
