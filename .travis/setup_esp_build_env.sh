d796cf07b171479e084d19f786f8e3377d3c5654
Aleksei Dynda <alexey.dynda@gmail.com>
2019-11-14 13:32:46 +1000

    Added ESP-IDF env

---
+++ .travis/setup_esp_build_env.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ d ~ /esp/esp-idf ]; then
+    echo "ESP IDF is already installed"
+    cd ~/esp/esp-idf
+    . export.sh
+    exit 0
+fi
+
+if [ "$1" = "travis" ]; then
+    echo "Installing packages..."
+    # sudo apt-get install git wget libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-click python-cryptography python-future python-pyparsing python-pyelftools cmake ninja-build ccache
+fi
+
+mkdir -p ~/esp
+git clone --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
+cd ~/esp/esp-idf
+if [ "$1" = "travis" ]; then
+    ./install.sh
+fi
+. export.sh
