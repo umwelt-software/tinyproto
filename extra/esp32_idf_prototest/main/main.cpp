cc4273dd54010797133f7224741b1f7d3163a003
Aleksei Dynda <alexey.dynda@gmail.com>
2019-11-14 14:51:07 +1000

    Adding cache

--- extra/esp32_idf_prototest/main/main.cpp
+++ extra/esp32_idf_prototest/main/main.cpp
@@ -1,10 +1,31 @@
 /*
- * This example sends <Hello> packet every second.
- * This is demontraction of Half Duplex protocol.
- */
+    MIT License
+
+    Copyright (c) 2018 - 2019, Alexey Dynda
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include <TinyProtocolHd.h>
 #include <stdio.h>
-#include <esp_task_wdt.h>
 
 /* Function to receive incoming messages from remote side */
 void onFrameIn(uint8_t *buf, int len)
@@ -20,14 +41,10 @@ uint8_t buffer[64];
 Tiny::ProtoHd  proto(buffer, sizeof(buffer), onFrameIn);
 
 void setup() {
-    /* No timeout, since we want non-blocking UART operations */
-    Serial.setTimeout(0);
-    /* Initialize serial protocol for test purposes */
-    Serial.begin(115200);
     /* We do want to use simple checkSum */
     proto.enableCheckSum();
     /* Redirect all protocol communication to Serial0 UART */
-    proto.beginToSerial();
+//    proto.beginToSerial();
 }
 
 void loop()
@@ -35,15 +52,30 @@ void loop()
     /* Prepare data you want to send here */
     Tiny::Packet<6> packet;
     packet.put( "HELLO" );
-
     /* Send packet over UART to other side */
     proto.write(packet);
+    proto.run();
+}
 
-    /* We send HELLO packet every 1 second */
-    unsigned long start = millis();
-    while (millis() - start < 1000)
-    {
-        proto.run();
+
+extern void setup(void);
+extern void loop(void);
+
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
+
+void main_task(void *args)
+{
+    setup();
+    for(;;) {
+        loop();
     }
-//    esp_task_wdt_reset();
+}
+
+extern "C" void app_main(void)
+{
+    xTaskCreatePinnedToCore(main_task, "mainTask", 8192, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
 }
