b11836a636e62c49a302e645e5d6b6a24ce0133e
Aleksei Dynda <alexey.dynda@gmail.com>
2019-11-14 15:08:05 +1000

    Fixing cached esp32 setup

--- .travis/setup_esp_build_env.sh
+++ .travis/setup_esp_build_env.sh
@@ -3,15 +3,15 @@
 if [ -d ~/esp/esp-idf ]; then
     echo "ESP IDF is already installed"
     exit 0
-fi
-
-if [ "$1" = "travis" ]; then
-    echo "Installing packages..."
+else
+    # echo "Installing packages..."
     # sudo apt-get install libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-click python-cryptography python-future python-pyparsing python-pyelftools cmake ninja-build ccache
+    #
+    mkdir -p ~/esp
+    git clone --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
 fi
 
-mkdir -p ~/esp
-git clone --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
+
 cd ~/esp/esp-idf
 if [ "$1" = "travis" ]; then
     ./install.sh
