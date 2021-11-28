1e1f8f69ff8e696d2c900481295075513b9d9f90
lexus2k <alexey.dynda@gmail.com>
2021-11-28 13:02:07 +1000

    Fixing CI-configuration files

--- .travis/setup_esp_build_env.sh
+++ .travis/setup_esp_build_env.sh
@@ -6,8 +6,10 @@ else
     # echo "Installing packages..."
     # sudo apt-get install libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-click python-cryptography python-future python-pyparsing python-pyelftools cmake ninja-build ccache
     #
+    # Fixing version to v4.3 since v5.0 doesn't support Makefiles anymore
     mkdir -p ~/esp
-    git clone --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
+    git clone -b v4.3 --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
+    # git clone --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
 fi
 
 cd ~/esp/esp-idf
